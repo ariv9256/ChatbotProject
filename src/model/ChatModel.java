85d7fb09d6ae42d7e466c68cf22e87b1c0ec4d64
Angela Rivas <snowcats001@gmail.com>
2018-10-16 13:31:47 -0600

    Added response list for response & spooky lists, removed "favoriteHoliday" variable (can add it as a question, not necessary).

--- src/model/ChatModel.java
+++ src/model/ChatModel.java
@@ -4,9 +4,8 @@ import java.util.ArrayList;
 
 public class ChatModel
 {
-	private String favoriteHoliday;
-	private ArrayList<ChatModel> responseList = new ArrayList<ChatModel>();
-	private ArrayList<ChatModel> spookyList = new ArrayList<ChatModel>();
+	private ArrayList<String> responseList = new ArrayList<String>();
+	private ArrayList<String> spookyList = new ArrayList<String>();
 	private String content; 
 	private String currentUser;
 	private String joke;
@@ -15,25 +14,39 @@ public class ChatModel
 	{
 		//Default Constructor
 	}
-	public ChatModel(String favoriteHoliday, ArrayList<ChatModel> responseList, 
+	public ChatModel(ArrayList<ChatModel> responseList, 
 			ArrayList<ChatModel> spookyList, String content, String currentUser, String joke)
 	{
-		this.favoriteHoliday = favoriteHoliday;
-		this.responseList = responseList;
-		this.spookyList = spookyList;
-		this.content = content;
-		this.currentUser = currentUser;
-		this.joke = joke;
+		this.responseList = new ArrayList<String>();
+		this.spookyList = new ArrayList<String>();
+		this.content = new String("Default spoop :(");
+		this.currentUser = new String("Default empty spooky");
+		this.joke = "What instrument does a skeleton play?";
+		
+		buildTheLists();
+	
 	}
-	public String getfavoriteHoliday()
+	private void buildTheLists()
 	{
-		return favoriteHoliday;
+		responseList.add("Hello there! How are you in this lovely evening?");
+		responseList.add("That is mean goodbye :( Leave me alone");
+		responseList.add("What is your name?");
+		responseList.add("What is your favorite holiday?");
+		responseList.add("I can't do the understanding of that statement");
+		responseList.add("What are you doing?");
+		
+		spookyList.add("What is your favorite holiday(it should be obvious).");
+		spookyList.add("Jack Skellington is the bestest!");
+		spookyList.add("B O O");
+		spookyList.add("If you were to be a halloween monster, what would it be?");
+		spookyList.add("Cause this is thriller!");
+		spookyList.add("Would you like to do the trick or treat?");
 	}
-	public ArrayList<ChatModel> getResponseList()
+	public ArrayList<String> getResponseList()
 	{
 		return responseList;
 	}
-	public ArrayList<ChatModel> getSpookyList()
+	public ArrayList<String> getSpookyList()
 	{
 		return spookyList;
 	}
@@ -49,15 +62,11 @@ public class ChatModel
 	{
 		return joke;
 	}
-	public void setFavoriteHoliday(String favoriteHoliday)
-	{
-		this.favoriteHoliday = favoriteHoliday;
-	}
-	public void setResponseList(ArrayList<ChatModel> responseList)
+	public void setResponseList(ArrayList<String> responseList)
 	{
 		this.responseList = responseList;
 	}
-	public void setSpookyList(ArrayList<ChatModel> spookyList)
+	public void setSpookyList(ArrayList<String> spookyList)
 	{
 		this.spookyList = spookyList;
 	}
