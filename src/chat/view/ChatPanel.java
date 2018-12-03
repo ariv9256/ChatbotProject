9f818fd982b51faeabfa8fef990c360623aafcb2
Angela Rivas <snowcats001@gmail.com>
2018-12-03 13:18:08 -0700

    added chatpanel and chatframe classes in chat.view package, added import statements.

---
+++ src/chat/view/ChatPanel.java
@@ -0,0 +1,11 @@
+package chat.view;
+
+import javax.swing*;
+import chat.controller.ChatController;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChatPanel {
+
+}
