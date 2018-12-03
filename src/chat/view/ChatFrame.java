46b387f0ed496d09d318912bd743815dfb23b410
Angela Rivas <snowcats001@gmail.com>
2018-12-03 13:55:14 -0700

    fixed other errors in jframe

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -3,7 +3,7 @@ package chat.view;
 import javax.swing.JFrame;
 import chat.controller.ChatController;
 
-public class ChatFrame 
+public class ChatFrame extends JFrame
 {
 	private ChatController appController;
 	private ChatPanel appPanel;
