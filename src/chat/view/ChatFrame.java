1e89409070d1a7ed068358475047acc43bd0b954
Angela Rivas <snowcats001@gmail.com>
2018-12-03 13:33:44 -0700

    Added layouts, panel, and listener under ChatPanel. proper set ups so far

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,5 +1,27 @@
 package chat.view;
 
-public class ChatFrame {
+import javax.swing.JFrame;
+import chat.controller.ChatController;
 
+public class ChatFrame 
+{
+	private ChatController appController;
+	private ChatPanel appPanel;
+	
+	public ChatFrame()
+	{
+		super();
+		this.appController = appController;
+		this.appPanel = new ChatPanel(appController);
+		
+		setupFrame();
+	}
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setSize(800, 800);
+		this.setTitle("Hello there family :)");
+		this.setResizable(true);
+		this.setVisible(true);
+	}
 }
--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,11 +1,49 @@
 package chat.view;
 
-import javax.swing*;
+import javax.swing.* ;
 import chat.controller.ChatController;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ChatPanel {
-
+public class ChatPanel extends JPanel 
+{
+	private ChatController appController;
+	private ChatPanel appPanel;
+	private JLabel firstLabel;
+	private JButton firstButton;
+	private SpringLayout appLayout;
+	
+	public ChatPanel(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		firstLabel = new JLabel("Hello :)");
+		firstButton = new JButton("Boop!");
+		appLayout = new SpringLayout();
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	private void changeBackground()
+	{
+		
+	}
+	private void setupPanel()
+	{
+		this.setLayout(appLayout);
+		this.add(firstButton);
+		this.add(firstLabel);
+	}
+	private void setupLayout()
+	{
+		
+	}
+	private void setupListeners()
+	{
+		
+	}
+	
 }
