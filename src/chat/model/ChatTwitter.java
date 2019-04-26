252828b75a0da49a73d53b81afa30a86ebf1532c
Angela Rivas <snowcats001@gmail.com>
2019-04-26 13:43:48 -0600

    Added imports, initializiation, and constructors for the new 4 variables

--- src/chat/model/ChatTwitter.java
+++ src/chat/model/ChatTwitter.java
@@ -1,16 +1,41 @@
 package chat.model;
 import chat.controller.ChatController;
+import chat.controller.IOController;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.text.DecimalFormat;
+
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
+import twitter4j.TwitterException;
+import twitter4j.GeoLocation;
+import twitter4j.Paging;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.ResponseList;
+import twitter4j.Status;
+
 
 public class ChatTwitter 
 {
 	private ChatController app;
 	private Twitter chatTwitter;
+	private List<Status> searchedTweets;
+	private List<String> tweetedWords;
+	private long totalWordCount;
+	private HashMap<String, Integer> wordsAndCount;
 	
 	public ChatTwitter(ChatController app)
 	{
 		this.app = app;
 		this.chatTwitter = TwitterFactory.getSingleton();
+		this.searchedTweets = new ArrayList<Status>();
+		this.tweetedWords = new ArrayList<String>();
+		this.wordsAndCount = new HashMap<String, Integer>();
+		this.totalWordCount = 0;
 	}
 }
